21876e0e9aeb6d2c4c48caa1aa09ac8b6354222d
lilys2001 <46876744+lilys2001@users.noreply.github.com>
2019-04-25 16:22:22 -0700

    loading js is working, but stops the cs animation

---
+++ assets/scripts/script.js
@@ -0,0 +1,23 @@
+setTimeout(function() {
+        document.getElementById('load').src = 'assets/images/Load' + 0 + '.gif';
+}, 1000 * 0);
+
+setTimeout(function() {
+        document.getElementById('load').src = 'assets/images/Load' + 1 + '.gif';
+}, 1000 * 1);
+
+setTimeout(function() {
+        document.getElementById('load').src = 'assets/images/Load' + 2 + '.gif';
+}, 1000 * 2);
+
+setTimeout(function() {
+        document.getElementById('load').src = 'assets/images/Load' + 3 + '.gif';
+}, 1000 * 3);
+
+setTimeout(function() {
+        document.getElementById('load').src = 'assets/images/Load' + 4 + '.gif';
+}, 1000 * 4);
+
+setTimeout(function() {
+        document.getElementById('load').src = 'assets/images/Load' + 5 + '.gif';
+}, 1000 * 5);
