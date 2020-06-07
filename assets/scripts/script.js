c20d92d85983ea80fa76130b87fedbad7509dfd8
Lily Sai <sai.lily01@gmail.com>
2020-06-07 13:17:24 -0700

    removed old website

--- assets/scripts/script.js
+++
@@ -1,9 +0,0 @@
-for (i = 0; i < 6; i++) {
-    loadHelper(i);
-}
-
-function loadHelper(i) {
-    setTimeout(function() {
-            document.getElementById('load').src = 'assets/images/Load' + i + '.gif';
-    }, 1000 * i);
-};
